8b9ddb759b3e16384789ce70aef33813d5cf8815
ziyuzheng02 <123010670+ziyuzheng02@users.noreply.github.com>
2023-10-10 22:21:59 +0100

    Update energy_to_tof.py
    Added: 1) intergrated function for DT reaction (input temperature output neutron spectrum) just for convenience; 2) temperature vs mean energy plot

--- energy_to_tof.py
+++ energy_to_tof.py
@@ -59,3 +59,71 @@ plt.legend([dist])
 mean = np.average(bin_centers,weights=spectrum)
 std = np.sqrt(np.average((bin_centers-mean)**2,weights=spectrum))
 print(2.355*std)
+
+#%%
+# varying temperature for spectrum
+temp_list = [2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0]
+samples = 2e6
+for temp in temp_list:
+    reaction = dress.reactions.DTNHe4Reaction()
+    spec_calc = dress.SpectrumCalculator(reaction, n_samples=samples)
+    
+    particle_a = spec_calc.reactant_a.particle
+    Ta = temp
+    dist_a = dress.dists.MaxwellianDistribution(Ta, particle_a)
+    
+    particle_b = spec_calc.reactant_b.particle
+    Tb = temp
+    dist_b = dress.dists.MaxwellianDistribution(Tb, particle_b)
+    
+    spec_calc.reactant_a.v = dist_a.sample(spec_calc.n_samples)
+    spec_calc.reactant_b.v = dist_b.sample(spec_calc.n_samples)
+    
+    spec_calc.u = [0,0,1]
+    spectrum, bin_centers = spec_calc(bin_width=15.0)
+    
+    plt.plot(bin_centers, spectrum)
+plt.legend(temp_list)
+plt.xlabel('Neutron energy (keV)')
+plt.ylabel('Spectrum (au)')
+
+#%%
+#takes a temperature, returns neutron energy spectrum (y,x)
+def DT_nSpec(temp,n_samples=1e6,bin_width=25.0,u=[0,0,1],plot=False):
+    reaction = dress.reactions.DTNHe4Reaction()
+    spec_calc = dress.SpectrumCalculator(reaction, n_samples=n_samples)
+
+    particle_a = spec_calc.reactant_a.particle
+    Ta = temp
+    dist_a = dress.dists.MaxwellianDistribution(Ta, particle_a)
+
+    particle_b = spec_calc.reactant_b.particle
+    Tb = temp
+    dist_b = dress.dists.MaxwellianDistribution(Tb, particle_b)
+
+    spec_calc.reactant_a.v = dist_a.sample(spec_calc.n_samples)
+    spec_calc.reactant_b.v = dist_b.sample(spec_calc.n_samples)
+
+    spec_calc.u = u
+    spectrum, bin_centers = spec_calc(bin_width=bin_width)
+    
+    if plot:
+        plt.plot(bin_centers, spectrum)
+        plt.xlabel('Neutron energy (keV)')
+        plt.ylabel('Spectrum (au)')
+        
+    return spectrum, bin_centers
+
+spectrum, bin_centers = DT_nSpec(3.0, n_samples=1e7, bin_width=5.0, plot=True)
+
+#%%
+#T vs FWHM verified by @Binbing
+#T vs spectral mean
+temp_list = [2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0]
+mean_list = []
+for temp in temp_list:
+    spectrum, bin_centers = DT_nSpec(temp, n_samples=5e6, bin_width=5.0, plot=False)
+    mean_list.append(np.average(bin_centers,weights=spectrum))
+plt.plot(np.array(mean_list)-14100, temp_list, 'o', linestyle='-')
+plt.xlabel('Mean shift (keV)')
+plt.ylabel('Ion Temperature (keV)')
